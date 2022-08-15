39605d48f06723e0f19a1bc52448f417b8bf9da8
Tiemi Faustino <hayashipharma@gmail.com>
2022-08-15 17:08:12 -0300

    feat: :sparkles:
    Adicionando Rotas

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Switch>
+      <h2>Rotas</h2>
+      {/* <Route exact path='/projetos' component={ Projects } /> */}
+      {/* <Route exact path='/contato' component={ Contact } /> */}
+      {/* <Route exact path='/' component={ Home }/> */}
+      {/* <Route path="*" component={ NotFound } /> */}
+    </Switch>
   );
 }
 
