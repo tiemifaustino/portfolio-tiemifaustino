e7dbac4fc931d6e9c4415d28537f4919ceec1884
Tiemi Faustino <hayashipharma@gmail.com>
2022-08-16 15:05:24 -0300

    feat: :sparkles:
    Adicionando conteúdo da página Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
+import imageHome from '../images/tiemi-home.png';
 
 export default function Home() {
   return (
     <>
       <Header />
+      <main>
+        <div>
+          <span>Olá, eu sou</span>
+          <h1>Tiemi Faustino</h1>
+          <h3>Desenvolvedora Web Full Stack</h3>
+        </div>
+        <img src={imageHome} alt="Foto de Tiemi Faustino" width="400px" />
+      </main>
       <Footer />
     </>
   );
