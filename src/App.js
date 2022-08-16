64bf2e651e0eb94d840fd5506e0a0aa512bc46f0
Tiemi Faustino <hayashipharma@gmail.com>
2022-08-16 10:18:12 -0300

    feat: :sparkles:
    Criando página Home

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import './App.css';
 import { Route, Switch } from 'react-router-dom';
+import Home from './pages/Home';
+import React from 'react';
 
 function App() {
   return (
     <Switch>
-      <h2>Rotas</h2>
       {/* <Route exact path='/sobre' component={ About } /> */}
       {/* <Route exact path='/projetos' component={ Projects } /> */}
       {/* <Route exact path='/contato' component={ Contact } /> */}
-      {/* <Route exact path='/' component={ Home }/> */}
+      <Route exact path='/' component={ Home }/>
       {/* <Route path="*" component={ NotFound } /> */}
     </Switch>
   );
