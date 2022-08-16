5a0eb2312587787c270750311169819fdb4b1890
Tiemi Faustino <hayashipharma@gmail.com>
2022-08-16 16:33:12 -0300

    feat: :sparkles: Adicionando página Projetos

---
+++ src/pages/Projects.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import Footer from '../components/Footer';
+import Header from '../components/Header';
+
+export default function Projects() {
+  return (
+    <>
+      <Header />
+      <main>
+        <h1>Projetos</h1>
+        <div>
+          <div>
+            <div>
+              Imagem
+            </div>
+            <div>
+              <h3>Título</h3>
+              <span>Frontend</span>
+              <p>
+                Descrição Lorem, ipsum dolor sit amet consectetur
+                adipisicing elit. Optio nisi vero sunt.
+                Tempora voluptatem nulla explicabo veniam magnam
+                illum cupiditate vitae sunt praesentium ea dignissimos
+                est, modi totam quas molestias!
+              </p>
+            </div>
+          </div>
+        </div>
+      </main>
+      <Footer />
+    </>
+  );
+}
